2747f37874a6dfac045a0a68f3912e9bee4e2af5
Enip <a23956491z@yahoo.com.tw>
2021-01-10 21:01:47 +0800

    寫完三均線的策略

--- main.py
+++ main.py
@@ -1,121 +1,17 @@
-
-# %%
 import pandas as pd
-import os
 import numpy as np
-import matplotlib.pyplot as plt
-import talib
-import mplfinance as mpf
-
-# %%
-
-def preprocess(file):
-
-    df = pd.read_excel(file, engine='openpyxl', parse_dates=True, header=None)
-    colume_name = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
-    df.columns = colume_name
-    df = df.set_index('Date')
-
-    for col in df.columns:
-        if(col=='Date'):
-            continue;
-        df[col] = np.array([float(x) for x in df[col]])
-
-    return df
-
-ticker = preprocess('data/1325.xlsx')
-print(ticker)
-
-# %%
-class trade:
-
-    __comission = 0
-    def __init__(self, ticker ,comission=0, tax=0, holding_limit=0,
-                    print_trading=False, print_returnRate=False, print_tradingTimes=False):
-
-
-        self.comission = comission
-        self.tax = tax
-        self.ticker = ticker
-        self.holding_limit = holding_limit
-
-        self.print_trading = print_trading
-        self.print_returnRate = print_returnRate
-        self.print_tradingTimes = print_tradingTimes
-
-        self.principal = 0
-        self.balance = 0
-        self.holding_tickers = 0
-
-        self.trading_nums = 0
-
-    def buy(self):
-        if((self.holding_limit == 0) or
-            self.holding_tickers < self.holding_limit):
-
-            if self.print_trading:
-                print('\t{} buy  {}'.format(self.date,self.position['Close']))
-
-            if(self.principal == 0):
-                self.principal = self.position['Close']
-
-            self.balance -= self.position['Close'] * (1+self.comission)
-            self.holding_tickers = self.holding_tickers + 1
-
-        return None
-
-    def sell(self):
-        if(self.holding_tickers):
-            if self.print_trading:
-                print('\t{} sell {}'.format(self.date,self.position['Close']))
-
-            self.balance += self.position['Close'] * (1 - self.comission - self.tax)
-            self.holding_tickers = self.holding_tickers - 1;
-
-            self.trading_nums = self.trading_nums+1
-        return None
-    def next(self):
-
-        self.buy()
-        self.buy()
-        self.sell()
-        return None
-
-    def run(self):
-        for index, row in self.ticker.iterrows():
-            self.position = row
-            self.date = index
-
-            close_price = self.position['Close']
-            self.next()
-
-        for i in range(self.holding_tickers):
-            self.sell()
-
-    def result(self):
-        if(self.principal):
-            return_rate = self.balance / self.principal
-            return_rate = round(return_rate*100, 4)
-
-            if self.print_tradingTimes:
-                print("\ttrading times : ", self.trading_nums)
-
-            if self.print_returnRate:
-                print("\treturn rate : {r}%".format(r=return_rate))
-
-            return return_rate
-        return None
-
-
-df = preprocess('data/1325.xlsx')
-hey = trade(df, 0.001425, 0.003, 1)
-hey.run()
-
-print("return rate : {r}%".format(r=hey.result()))
-
-# %%
 import glob
 
+from tools import trade
+from tools import readStock_excel
+
+from strategy import *
+
+print_format = {'tradingRecord'     : True,
+                'tradingNum'        : True,
+                'fileName'          : True,
+                'fileNameNewLine'   : True,
+                'returnRate'        : True}
 
 class derieved(trade):
 
@@ -126,43 +22,22 @@ class derieved(trade):
             self.sell()
 
 
-returnRates = {};
+returnRates = {}
 for data_file in glob.glob("data/*.xlsx"):
+    if print_format['fileName']:
+        print(data_file, end='\n' if print_format['fileNameNewLine'] else '')
+    ticker = readStock_excel(data_file)
 
-    print(data_file)
-    ticker = preprocess(data_file)
+    dw = strategy3(ticker,7,15,21)
 
-    short_window = 9
-    long_window = 9
-    dw = ticker
-    dw['slowk'], dw['slowd'] = talib.STOCH(
-    			ticker['High'].values,
-    			ticker['Low'].values,
-    			ticker['Close'].values,
-                            fastk_period=short_window,
-                            slowk_period=long_window,
-                            slowk_matype=0.0,
-                            slowd_period=long_window,
-                            slowd_matype=0.0)
-
-
-    signals = pd.DataFrame(index=dw.index)
-    signals['signal'] = 0.0
-    signals['slowk'] = dw['slowk']
-    signals['slowd'] = dw['slowd']
-    signals['signal'][short_window:]  = np.where((signals['slowk'][short_window:]
-                                                > signals['slowd'][short_window:]), 1.0, 0.0)
-    signals['positon'] = signals['signal'].diff()
-    signals['buy'] = np.where((signals['slowk'] > 70) & (signals['positon'] == 1 ), 1.0, 0.0)
-    signals['sell'] = np.where((signals['slowk'] < 30) & (signals['positon'] == -1 ), 1.0, 0.0)
-
-    dw['buy'] = signals['buy']
-    dw['sell'] = signals['sell']
-    hey = derieved(dw, 0.001425, 0.003, 1, print_trading=True,print_tradingTimes=True, print_returnRate=True)
-    hey.run()
-    res = hey.result()
-    if res:
-        returnRates[data_file] = res
+    bakctesting = derieved(dw, 0.001425, 0.003, 1,
+                    print_trading=print_format['tradingRecord'],
+                    print_tradingTimes=print_format['tradingNum'],
+                    print_returnRate=print_format['returnRate'])
+    bakctesting.run()
+    result = bakctesting.result()
+    if result:
+        returnRates[data_file] = result
 
 arr = np.fromiter(returnRates.values(), dtype=float)
 
---
+++ strategy.py
@@ -0,0 +1,170 @@
+import pandas as pd
+import talib
+import numpy as np
+
+RSVn = 9
+RSVt = 3
+Kt = 3
+upperBound = 70
+lowerBound = 30
+
+stop_loss = 0.2
+def moving_stop_loss(dw, stop_loss = 0.2):
+
+    holding = 0
+    high = 0
+    dw['sell'] = np.zeros(dw.shape[0])
+    for index, row in dw.iterrows():
+        if row['buy'] == 1:
+            holding = 1
+
+        if holding:
+            if row['Close'] < high*(1-stop_loss) :
+                row['sell'] = 1
+
+                #print('sell : ', row['Close'] ,'high : ', high)
+                holding = 0
+                high = 0
+            high = max(row['Close'], high)
+            #print(high)
+    return dw
+
+
+def strategy1(ticker):
+
+
+    dw = ticker
+    dw['slowk'], dw['slowd'] = talib.STOCH(
+    			ticker['High'].values,
+    			ticker['Low'].values,
+    			ticker['Close'].values,
+                            fastk_period=RSVn, #RSV day
+                            slowk_period=RSVt,
+                            slowk_matype=0.0,
+                            slowd_period=Kt,
+                            slowd_matype=0.0)
+
+
+    signals = pd.DataFrame(index=dw.index)
+    signals['signal'] = 0.0
+    signals['slowk'] = dw['slowk']
+    signals['slowd'] = dw['slowd']
+    signals['signal'][RSVn:]  = np.where((signals['slowk'][RSVn:]
+                                                > signals['slowd'][RSVn:]), 1.0, 0.0)
+    signals['positon'] = signals['signal'].diff()
+    signals['buy'] = np.where((signals['slowk'] > upperBound) & (signals['positon'] == 1 ), 1.0, 0.0)
+    signals['sell'] = np.where((signals['slowk'] < lowerBound) & (signals['positon'] == -1 ), 1.0, 0.0)
+
+    dw['buy'] = signals['buy']
+    dw['sell'] = signals['sell']
+
+    return dw
+
+def strategy2(ticker):
+
+    dw = ticker
+    dw['slowk'], dw['slowd'] = talib.STOCH(
+    			ticker['High'].values,
+    			ticker['Low'].values,
+    			ticker['Close'].values,
+                            fastk_period=RSVn,
+                            slowk_period=RSVt,
+                            slowk_matype=0.0,
+                            slowd_period=Kt,
+                            slowd_matype=0.0)
+
+
+    signals = pd.DataFrame(index=dw.index)
+    signals['signal'] = 0.0
+    signals['slowk'] = dw['slowk']
+    signals['slowd'] = dw['slowd']
+    signals['signal'][RSVn:]  = np.where((signals['slowk'][RSVn:]
+                                                > signals['slowd'][RSVn:]), 1.0, 0.0)
+
+    signals['positon'] = signals['signal'].diff()
+    signals['buy'] = np.where((signals['slowk'] > upperBound) & (signals['positon'] == 1 ), 1.0, 0.0)
+    dw['buy'] = signals['buy']
+
+    dw = moving_stop_loss(dw, 0.2)
+    return dw
+
+
+ma_window_short, ma_window_mid,ma_window_long = 5,15,30
+def strategy3(  ticker,
+                ma_window_short = 7,
+                ma_window_mid = 15,
+                ma_window_long = 21,
+                tolerence_interval = 7):
+    def crossover(i , a, b):
+        if (((dw.iloc[i][a] > dw.iloc[i][b]) and (dw.iloc[i-1][a] < dw.iloc[i-1][b])) and (dw.iloc[i][a] > dw.iloc[i-1][a])):
+            return 1;
+    dw = ticker
+    dw['ma_short'] = dw['Close'].rolling(ma_window_short).mean()
+    dw['ma_mid'] = dw['Close'].rolling(ma_window_mid).mean()
+    dw['ma_long'] = dw['Close'].rolling(ma_window_long).mean()
+
+    dw['buy'] = np.zeros(dw.shape[0])
+    if dw.shape[0] > ma_window_long:
+
+        last_cross_mid = tolerence_interval
+        last_cross_long = tolerence_interval
+        for i in range(ma_window_long, dw.shape[0]):
+
+            if crossover(i, 'ma_short', 'ma_mid'):
+                last_cross_mid = 0
+            if crossover(i, 'ma_short', 'ma_long'):
+                last_cross_long = 0
+
+            if (crossover(i, 'ma_mid', 'ma_long') and
+                last_cross_mid < tolerence_interval and
+                last_cross_long < tolerence_interval) :
+
+                dw['buy'][i] =  1
+                #print(dw.index[i])
+                last_cross_mid = tolerence_interval
+                last_cross_long = tolerence_interval
+
+            last_cross_mid = last_cross_mid+1
+            last_cross_long = last_cross_long+1
+
+
+    dw = moving_stop_loss(dw, 0.2)
+
+
+    return dw
+
+
+if __name__ == '__main__':
+    from tools import readStock_excel
+    import matplotlib.pyplot as plt
+    import mplfinance as mpf
+
+
+    def plot_KD(dw):
+        dw.index = pd.to_datetime(dw.index)
+
+        fig,axes = plt.subplots(3,1,figsize=(16,10))
+
+        dw[['slowk','slowd']].plot(ax=axes[1], grid=True)
+
+        axes[1].plot(dw.loc[dw.buy == 1.0].index,
+                 dw.slowk[dw.buy == 1.0],
+                 '^', markersize=10, color='m', alpha=0.5)
+        axes[1].plot(dw.loc[dw.sell == 1.0].index,
+                 dw.slowk[dw.sell == 1.0],
+                 'v', markersize=10, color='k', alpha=0.5)
+
+        axes[2].bar(ticker.index, ticker['Volume'])
+        mpf.plot(ticker,type='candle', ax=axes[0])
+        plt.show()
+
+    def plot_MA(dw):
+        mpf.plot(dw, type='candle' ,mav=(ma_window_short, ma_window_mid,ma_window_long))
+    ticker = readStock_excel('data/2327.xlsx')
+
+    dw = strategy3(ticker)
+
+    print(dw)
+    plot_MA(dw)
+
+    #plot_KD(dw)
