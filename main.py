a959bac9104e981e3519f2ded0225a88b813e04c
Enip <a23956491z@yahoo.com.tw>
2021-01-10 21:38:39 +0800

    完成威廉指標

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ for data_file in glob.glob("data/*.xlsx"):
         print(data_file, end='\n' if print_format['fileNameNewLine'] else '')
     ticker = readStock_excel(data_file)
 
-    dw = tripleMA_stopLoss(ticker,7,15,21)
+    dw = KDBuy_KDSell(ticker)
 
     bakctesting = derieved(dw, 0.001425, 0.003, 1,
                     print_trading=print_format['tradingRecord'],
--- strategy.py
+++ strategy.py
@@ -2,11 +2,7 @@ import pandas as pd
 import talib
 import numpy as np
 
-RSVn = 9
-RSVt = 3
-Kt = 3
-upperBound = 70
-lowerBound = 30
+
 
 stop_loss = 0.2
 def moving_stop_loss(dw, stop_loss = 0.2):
@@ -30,8 +26,12 @@ def moving_stop_loss(dw, stop_loss = 0.2):
     return dw
 
 
-def KDBuy_KDSell(ticker):
-
+def KDBuy_KDSell(ticker, RSVn = 9,
+                        RSVt = 3,
+                        Kt = 3,
+                        upperBound = 70,
+                        lowerBound = 30,
+                        short_stop_loss=True):
 
     dw = ticker
     dw['slowk'], dw['slowd'] = talib.STOCH(
@@ -44,57 +44,25 @@ def KDBuy_KDSell(ticker):
                             slowd_period=Kt,
                             slowd_matype=0.0)
 
+    dw['signal'] = 0.0
+    dw['signal'][RSVn:]  = np.where((dw['slowk'][RSVn:]
+                                                > dw['slowd'][RSVn:]), 1.0, 0.0)
+    dw['positon'] = dw['signal'].diff()
+    dw['buy'] = np.where((dw['slowk'] > upperBound) & (dw['positon'] == 1 ), 1.0, 0.0)
 
-    signals = pd.DataFrame(index=dw.index)
-    signals['signal'] = 0.0
-    signals['slowk'] = dw['slowk']
-    signals['slowd'] = dw['slowd']
-    signals['signal'][RSVn:]  = np.where((signals['slowk'][RSVn:]
-                                                > signals['slowd'][RSVn:]), 1.0, 0.0)
-    signals['positon'] = signals['signal'].diff()
-    signals['buy'] = np.where((signals['slowk'] > upperBound) & (signals['positon'] == 1 ), 1.0, 0.0)
-    signals['sell'] = np.where((signals['slowk'] < lowerBound) & (signals['positon'] == -1 ), 1.0, 0.0)
-
-    dw['buy'] = signals['buy']
-    dw['sell'] = signals['sell']
+    if short_stop_loss:
+        dw = moving_stop_loss(dw, 0.2)
+    else :
+        dw['sell'] = np.where((dw['slowk'] < lowerBound) & (dw['positon'] == -1 ), 1.0, 0.0)
 
     return dw
 
-def KDBuy_stopLoss(ticker):
-
-    dw = ticker
-    dw['slowk'], dw['slowd'] = talib.STOCH(
-    			ticker['High'].values,
-    			ticker['Low'].values,
-    			ticker['Close'].values,
-                            fastk_period=RSVn,
-                            slowk_period=RSVt,
-                            slowk_matype=0.0,
-                            slowd_period=Kt,
-                            slowd_matype=0.0)
-
-
-    signals = pd.DataFrame(index=dw.index)
-    signals['signal'] = 0.0
-    signals['slowk'] = dw['slowk']
-    signals['slowd'] = dw['slowd']
-    signals['signal'][RSVn:]  = np.where((signals['slowk'][RSVn:]
-                                                > signals['slowd'][RSVn:]), 1.0, 0.0)
-
-    signals['positon'] = signals['signal'].diff()
-    signals['buy'] = np.where((signals['slowk'] > upperBound) & (signals['positon'] == 1 ), 1.0, 0.0)
-    dw['buy'] = signals['buy']
-
-    dw = moving_stop_loss(dw, 0.2)
-    return dw
-
-
-ma_window_short, ma_window_mid,ma_window_long = 5,15,30
 def tripleMA_stopLoss(  ticker,
                 ma_window_short = 7,
                 ma_window_mid = 15,
                 ma_window_long = 21,
                 tolerence_interval = 7):
+
     def crossover(i , a, b):
         if (((dw.iloc[i][a] > dw.iloc[i][b]) and (dw.iloc[i-1][a] < dw.iloc[i-1][b])) and (dw.iloc[i][a] > dw.iloc[i-1][a])):
             return 1;
@@ -133,13 +101,34 @@ def tripleMA_stopLoss(  ticker,
 
     return dw
 
+def WMR(ticker,n = 14, tolerence_interval = 3, upperBound = 80, lowerBound = 20, short_stop_loss = True):
+    dw = ticker
+
+    dw['current_high'] = dw['Close'].rolling(n).max()
+    dw['current_low'] = dw['Close'].rolling(n).min()
+    dw['W%R'] = (dw['current_high'] - dw['Close'])/(dw['current_high']-dw['current_low'])*100
+
+    dw['W%R_current_high'] =  dw['W%R'].rolling(tolerence_interval).max()
+    dw['W%R_current_low'] =  dw['W%R'].rolling(tolerence_interval).min()
+
+    dw['buy'] = np.where((dw['W%R'] > upperBound) & (dw['W%R'] == dw['W%R_current_high'] ), 1.0, 0.0)
+
+    if short_stop_loss:
+        dw = moving_stop_loss(dw, 0.2)
+    else :
+        dw['sell'] = np.where((dw['W%R'] < lowerBound) & (dw['W%R'] == dw['W%R_current_low'] ), 1.0, 0.0)
+
+    return dw
+
 
 if __name__ == '__main__':
     from tools import readStock_excel
     import matplotlib.pyplot as plt
     import mplfinance as mpf
 
-
+    ma_window_short = 7
+    ma_window_mid = 15
+    ma_window_long = 21
     def plot_KD(dw):
         dw.index = pd.to_datetime(dw.index)
 
@@ -160,11 +149,12 @@ if __name__ == '__main__':
 
     def plot_MA(dw):
         mpf.plot(dw, type='candle' ,mav=(ma_window_short, ma_window_mid,ma_window_long))
+
+
     ticker = readStock_excel('data/2327.xlsx')
 
     dw = tripleMA_stopLoss(ticker)
-
-    print(dw)
     plot_MA(dw)
+    print(dw.to_string())
 
     #plot_KD(dw)
