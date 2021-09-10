b8b15a107961990fae7dba9e314e5b9ec9b616b3
Enip Yu <a23956491z@yahoo.com.tw>
2021-09-10 06:19:30 +0000

    Jupyter start server script : Activate .env before start

--- jupyter-start.sh
+++ jupyter-start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+. /home/divclab/Desktop/Enip/env_backtesting/bin/activate
 jupyter lab --NotebookApp.token=e58441c7c39d8eef1ad1557c0b35eb387204717b771591ed --ip=134.208.3.186 --port=8880 --no-browser
